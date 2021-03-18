90362919b406f2675436ed0750d057dba1385d6f
Sandro Koll <sandro.koll@x-cellent.com>
2021-03-18 15:24:39 +0100

    upper case

--- cmd/machine.go
+++ cmd/machine.go
@@ -816,7 +816,7 @@ func machineUpdateFirmware(driver *metalgo.Driver, kind metalgo.FirmwareKind, ma
 			}
 		}
 		if !containsRevision(f, kind, vendor, board, currentVersion) {
-			fmt.Printf("---\nCurrent %s version: %s\n", kind, currentVersion)
+			fmt.Printf("---\nCurrent %s version: %s\n", strings.ToUpper(string(kind)), currentVersion)
 		}
 	}
 
