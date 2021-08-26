039cdafb81a4781b21419a723046b21161277b70
Gerrit <tirreg91@googlemail.com>
2021-08-26 12:09:18 +0200

    Correct refs.

--- cmd/issue.go
+++ cmd/issue.go
@@ -29,17 +29,17 @@ var (
 	IssueNoPartition = Issue{
 		ShortName:   "no-partition",
 		Description: "machine with no partition",
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#no-partition",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#no-partition",
 	}
 	IssueLivelinessDead = Issue{
 		ShortName:   "liveliness-dead",
 		Description: "the machine is not sending events anymore",
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#liveliness-dead",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#liveliness-dead",
 	}
 	IssueLivelinessUnknown = Issue{
 		ShortName:   "liveliness-unknown",
 		Description: "the machine is not sending LLDP alive messages anymore",
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#liveliness-unknown",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#liveliness-unknown",
 	}
 	IssueLivelinessNotAvailable = Issue{
 		ShortName:   "liveliness-not-available",
@@ -48,32 +48,32 @@ var (
 	IssueFailedMachineReclaim = Issue{
 		ShortName:   "failed-machine-reclaim",
 		Description: "machine phones home but not allocated",
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#failed-machine-reclaim",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#failed-machine-reclaim",
 	}
 	IssueIncompleteCycles = Issue{
 		ShortName:   "incomplete-cycles",
 		Description: fmt.Sprintf("machine has an incomplete lifecycle (%s)", circle),
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#incomplete-cycles",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#incomplete-cycles",
 	}
 	IssueASNUniqueness = Issue{
 		ShortName:   "asn-not-unique",
 		Description: "The ASN is not unique (only impact on firewalls)",
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#asn-not-unique",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#asn-not-unique",
 	}
 	IssueBMCWithoutMAC = Issue{
 		ShortName:   "bmc-without-mac",
 		Description: "BMC has no mac address",
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#bmc-without-mac",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#bmc-without-mac",
 	}
 	IssueBMCWithoutIP = Issue{
 		ShortName:   "bmc-without-ip",
 		Description: "BMC has no ip address",
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#bmc-without-ip",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#bmc-without-ip",
 	}
 	IssueNonDistinctBMCIP = Issue{
 		ShortName:   "bmc-no-distinct-ip",
 		Description: "BMC IP address is not distinct",
-		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot.html#bmc-no-distinct-ip",
+		RefURL:      "https://docs.metal-stack.io/stable/installation/troubleshoot/#bmc-no-distinct-ip",
 	}
 
 	AllIssues = Issues{
