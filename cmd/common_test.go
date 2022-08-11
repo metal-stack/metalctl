0a24584a2c762e2c0e867d36cc7ff06dcdaa0596
Gerrit <info@gerritschwerthelm.de>
2022-08-11 14:00:43 +0200

    Testing different output formats.

--- cmd/common_test.go
+++ cmd/common_test.go
@@ -3,11 +3,13 @@ package cmd
 import (
 	"encoding/json"
 	"io"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/metal-stack/metal-go/test/client"
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 	"gopkg.in/yaml.v3"
@@ -23,46 +25,93 @@ func newTestConfig(t *testing.T, out io.Writer, mocks *client.MetalMockFns) (*co
 	}, mock
 }
 
-func outputFormats[R any]() []outputFormat[R] {
+func outputFormats[R any](want R, table string) []outputFormat[R] {
 	return []outputFormat[R]{
-		&jsonOutputFormat[R]{},
-		&yamlOutputFormat[R]{},
+		&tableOutputFormat[R]{table: table},
+		&jsonOutputFormat[R]{want: want},
+		&yamlOutputFormat[R]{want: want},
 	}
 }
 
 type outputFormat[R any] interface {
 	Name() string
-	Validate(t *testing.T, output []byte, want R)
+	Validate(t *testing.T, output []byte)
 }
 
-type jsonOutputFormat[R any] struct{}
+type jsonOutputFormat[R any] struct {
+	want R
+}
 
 func (o *jsonOutputFormat[R]) Name() string {
 	return "json"
 }
 
-func (o *jsonOutputFormat[R]) Validate(t *testing.T, output []byte, want R) {
+func (o *jsonOutputFormat[R]) Validate(t *testing.T, output []byte) {
 	var got R
 	err := json.Unmarshal(output, &got)
 	require.NoError(t, err, string(output))
 
-	if diff := cmp.Diff(want, got); diff != "" {
+	if diff := cmp.Diff(o.want, got); diff != "" {
 		t.Errorf("diff (+got -want):\n %s", diff)
 	}
 }
 
-type yamlOutputFormat[R any] struct{}
+type yamlOutputFormat[R any] struct {
+	want R
+}
 
 func (o *yamlOutputFormat[R]) Name() string {
 	return "yaml"
 }
 
-func (o *yamlOutputFormat[R]) Validate(t *testing.T, output []byte, want R) {
+func (o *yamlOutputFormat[R]) Validate(t *testing.T, output []byte) {
 	var got R
 	err := yaml.Unmarshal(output, &got)
 	require.NoError(t, err, string(output))
 
-	if diff := cmp.Diff(want, got); diff != "" {
+	if diff := cmp.Diff(o.want, got); diff != "" {
 		t.Errorf("diff (+got -want):\n %s", diff)
+	}
+}
+
+type tableOutputFormat[R any] struct {
+	table string
+}
+
+func (o *tableOutputFormat[R]) Name() string {
+	return "table"
+}
+
+func (o *tableOutputFormat[R]) Validate(t *testing.T, output []byte) {
+	trimAll := func(ss []string) []string {
+		var res []string
+		for _, s := range ss {
+			res = append(res, strings.TrimSpace(s))
+		}
+		return res
+	}
+
+	var (
+		trimmedWant = strings.TrimSpace(o.table)
+		trimmedGot  = strings.TrimSpace(string(output))
+
+		wantRows = trimAll(strings.Split(trimmedWant, "\n"))
+		gotRows  = trimAll(strings.Split(trimmedGot, "\n"))
+	)
+
+	t.Logf("got following table output:\n%s\n", trimmedGot)
+	t.Log(cmp.Diff(trimmedWant, trimmedGot))
+
+	require.Equal(t, len(wantRows), len(gotRows), "tables have different lengths")
+
+	for i := range wantRows {
+		wantFields := trimAll(strings.Split(wantRows[i], " "))
+		gotFields := trimAll(strings.Split(gotRows[i], " "))
+
+		require.Equal(t, len(wantFields), len(gotFields), "table fields have different lengths")
+
+		for i := range wantFields {
+			assert.Equal(t, wantFields[i], gotFields[i])
+		}
 	}
 }
--- cmd/image_test.go
+++ cmd/image_test.go
@@ -20,6 +20,7 @@ func Test_ImageListCmd(t *testing.T) {
 		name       string
 		metalMocks *client.MetalMockFns
 		want       []*models.V1ImageResponse
+		wantTable  string
 		wantErr    error
 	}{
 		{
@@ -58,12 +59,17 @@ func Test_ImageListCmd(t *testing.T) {
 					Usedby:   []string{},
 				},
 			},
+			wantTable: `
+ID       NAME     DESCRIPTION   FEATURES   EXPIRATION   STATUS   USEDBY
+debian   debian                 machine                          0
+ubuntu   ubuntu                 machine                          0
+`,
 		},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			for _, format := range outputFormats[[]*models.V1ImageResponse]() {
+			for _, format := range outputFormats(tt.want, tt.wantTable) {
 				format := format
 				t.Run(format.Name(), func(t *testing.T) {
 					var out bytes.Buffer
@@ -77,7 +83,7 @@ func Test_ImageListCmd(t *testing.T) {
 						t.Errorf("error diff (+got -want):\n %s", diff)
 					}
 
-					format.Validate(t, out.Bytes(), tt.want)
+					format.Validate(t, out.Bytes())
 
 					mock.AssertExpectations(t)
 				})
