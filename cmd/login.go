3f5e2bbff959e619f10097230ca29a3f19bc978a
Stefan Majer <stefan.majer@f-i-ts.de>
2023-12-01 12:37:17 +0100

    Minimum client version support

--- cmd/login.go
+++ cmd/login.go
@@ -6,8 +6,11 @@ import (
 	"os"
 	"strings"
 
+	"github.com/Masterminds/semver/v3"
+	"github.com/metal-stack/metal-go/api/client/version"
 	"github.com/metal-stack/metal-lib/auth"
 	"github.com/metal-stack/metalctl/pkg/api"
+	"github.com/metal-stack/v"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -62,7 +65,42 @@ func newLoginCmd(c *config) *cobra.Command {
 
 			fmt.Fprintln(c.out)
 
-			return auth.OIDCFlow(config)
+			err := auth.OIDCFlow(config)
+			if err != nil {
+				return err
+			}
+
+			// We need to reread the written kubeconfig
+			err = initConfigWithViperCtx(c)
+			if err != nil {
+				return err
+			}
+
+			resp, err := c.client.Version().Info(version.NewInfoParams(), nil)
+			if err != nil {
+				return err
+			}
+			if resp.Payload != nil && resp.Payload.MinClientVersion != nil {
+				minVersion := *resp.Payload.MinClientVersion
+				parsedMinVersion, err := semver.NewVersion(minVersion)
+				if err != nil {
+					return fmt.Errorf("required metalctl minimum version:%q is not semver parsable:%w", minVersion, err)
+				}
+				// This is a developer build
+				if !strings.HasPrefix(v.Version, "v") {
+					return nil
+				}
+				thisVersion, err := semver.NewVersion(v.Version)
+				if err != nil {
+					return fmt.Errorf("metalctl version:%q is not semver parsable:%w", v.Version, err)
+				}
+				if thisVersion.LessThan(parsedMinVersion) {
+					return fmt.Errorf("your metalctl version:%s is smaller than the required minimum version:%s, please run `metalctl update do` to get the latest version", thisVersion, minVersion)
+				}
+			}
+
+			return nil
+
 		},
 	}
 	loginCmd.Flags().Bool("print-only", false, "If true, the token is printed to stdout")
--- cmd/update.go
+++ cmd/update.go
@@ -3,10 +3,11 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/metal-stack/metal-go/api/client/version"
 	"github.com/metal-stack/updater"
 )
 
-func newUpdateCmd() *cobra.Command {
+func newUpdateCmd(c *config) *cobra.Command {
 	updateCmd := &cobra.Command{
 		Use:   "update",
 		Short: "update the program",
@@ -15,7 +16,11 @@ func newUpdateCmd() *cobra.Command {
 		Use:   "check",
 		Short: "check for update of the program",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			u, err := updater.New("metal-stack", binaryName, binaryName)
+			desired, err := getDesiredVersion(c)
+			if err != nil {
+				return err
+			}
+			u, err := updater.New("metal-stack", binaryName, binaryName, desired)
 			if err != nil {
 				return err
 			}
@@ -26,7 +31,11 @@ func newUpdateCmd() *cobra.Command {
 		Use:   "do",
 		Short: "do the update of the program",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			u, err := updater.New("metal-stack", binaryName, binaryName)
+			desired, err := getDesiredVersion(c)
+			if err != nil {
+				return err
+			}
+			u, err := updater.New("metal-stack", binaryName, binaryName, desired)
 			if err != nil {
 				return err
 			}
@@ -36,4 +45,15 @@ func newUpdateCmd() *cobra.Command {
 	updateCmd.AddCommand(updateCheckCmd)
 	updateCmd.AddCommand(updateDoCmd)
 	return updateCmd
+}
+
+func getDesiredVersion(c *config) (*string, error) {
+	resp, err := c.client.Version().Info(version.NewInfoParams(), nil)
+	if err != nil {
+		return nil, err
+	}
+	if resp.Payload != nil && resp.Payload.MinClientVersion != nil {
+		return resp.Payload.MinClientVersion, nil
+	}
+	return nil, nil
 }
